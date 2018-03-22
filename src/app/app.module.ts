29ae816c24145e1eed10d6a9b5411d1a369bf698
PaulinaH5 <34092203+PaulinaH5@users.noreply.github.com>
2018-03-22 12:57:27 +0100

    working routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
+import { HomeModule } from './home/home.module';
+import { LibraryModule } from './library/library.module';
+import { PharmacyModule } from './pharmacy/pharmacy.module';
+import { BankModule } from './bank/bank.module';
+import { HealthFoodStoreModule } from './health-food-store/health-food-store.module';
+import { UniversityModule } from './university/university.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,6 +18,16 @@ import { PharmacyComponent } from './pharmacy/pharmacy.component';
 import { BankComponent } from './bank/bank.component';
 import { HealthFoodStoreComponent } from './health-food-store/health-food-store.component';
 import { UniversityComponent } from './university/university.component';
+
+const routes: Routes = [
+  // Fallback when no prior route is matched
+  { path: 'home',  component: HomeComponent },
+  { path: 'university',  component: HomeComponent },
+  { path: 'library',  component: LibraryComponent },
+  { path: 'bank',  component: BankComponent },
+  { path: 'health-food-store',  component: HealthFoodStoreComponent },
+  { path: 'pharmacy',  component: LibraryComponent },
+];
 
 @NgModule({
   declarations: [
@@ -22,9 +40,16 @@ import { UniversityComponent } from './university/university.component';
     UniversityComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    HomeModule,
+    LibraryModule,
+    PharmacyModule,
+    BankModule,
+    HealthFoodStoreModule,
+    UniversityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
