69cfe6f68ffa51b973325e206bfef9aa34094fcc
PaulinaH5 <34092203+PaulinaH5@users.noreply.github.com>
2018-04-28 17:47:14 +0200

    add color change function and color template in css

--- src/app/pharmacy/pharmacy.component.ts
+++ src/app/pharmacy/pharmacy.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PharmacyComponent implements OnInit {
 
+  public optionValue;
+
   constructor() { }
 
   ngOnInit() {
+  this.optionValue = {"optionA":true, "optionB":false, "optionC":false}
+  }
+
+  public GetOption(){
+    return this.optionValue;
+  }
+
+  public ColorOption(value:number){
+    if(value === 1){
+      this.optionValue = {"optionA":true, "optionB":false, "optionC":false}
+      console.log("option A");
+    }
+    if(value === 2){
+      this.optionValue = {"optionA":false, "optionB":true, "optionC":false}
+      console.log("option B");
+    }
+    if(value === 3){
+      this.optionValue = {"optionA":false, "optionB":false, "optionC":true}
+      console.log("option C");
+    }
   }
 
 }
